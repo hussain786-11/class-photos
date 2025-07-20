a776d8663552dd5acb15c5fb5c63dd195ef7adb2
Hussain <dhussain786.11@gmail.com>
2025-07-20 21:08:37 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const handleSubmit = () => {
     if (
       rollNumber.length !== 12 ||
-      !rollNumber.startsWith('2411it0100') && !rollNumber.startsWith('2411IT0100')
+      (!rollNumber.startsWith('2411it0100') && !rollNumber.startsWith('2411IT0100'))
     ) {
       alert('Please enter a valid roll number in the format 2411it0100(1-80)');
       setShowImage(false);
@@ -17,29 +17,58 @@ function App() {
     }
 
     const imgNum = rollNumber.substring(10);
-    
     setImageSrc(`${imgNum}.png`);
     setShowImage(true);
   };
+  const open=()=>{
+    const open=document.querySelector('.nav-icons')
+   
+      open.style.display='flex'
+    }
+    const close=()=>{
+      const close=document.querySelector('.nav-icons')
+        close.style.display='none'
+
+    }
+  
+
 
   return (
     <div className="container">
-      <div className="hussain">
-        <h1>Hussain</h1>
+      {/* Header with nav and logo */}
+      <div className="header">
+          <div className="hussain">Hussain</div>
+        <div className="nav-links">
+          <a className="b" href="https://www.instagram.com/d.hussain_786?igsh=MncyeWhlZmltN3d5">
+            Instagram
+          </a>
+          <a className="b" href="https://www.facebook.com/share/1CghLrLP1n/">
+            Facebook
+          </a>
+          <a className="b" href="https://wa.me/qr/S2EJIPAJNQQUG1">
+            WhatsApp
+          </a>
+        </div>
+        <div className='manu'>
+          <svg onClick={open} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        </div>
+        <div className="nav-icons">
+         <svg onClick={close} className='b' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+          <a className="b" href="https://www.instagram.com/d.hussain_786?igsh=MncyeWhlZmltN3d5">
+            Instagram
+          </a>
+          <a className="b" href="https://www.facebook.com/share/1CghLrLP1n/">
+            Facebook
+          </a>
+          <a className="b" href="https://wa.me/qr/S2EJIPAJNQQUG1">
+            WhatsApp
+          </a>
+           
+        </div>
+        
       </div>
 
-      <div className="a">
-        <a className="b" href="https://www.instagram.com/d.hussain_786?igsh=MncyeWhlZmltN3d5">
-          Instagram
-        </a>
-        <a className="b" href="https://www.facebook.com/share/1CghLrLP1n/">
-          Facebook
-        </a>
-        <a className="b" href="https://wa.me/qr/S2EJIPAJNQQUG1">
-          WhatsApp
-        </a>
-      </div>
-
+      {/* Form Section */}
       <div className="form-section">
         <center>
           <h2>Enter the roll number to view the image</h2>
@@ -58,7 +87,7 @@ function App() {
             <img
               id="myImage"
               src={process.env.PUBLIC_URL + `/${imageSrc}`}
-              alt="Your Image"
+              alt="Roll Image"
               style={{ maxWidth: '300px' }}
             />
           )}
